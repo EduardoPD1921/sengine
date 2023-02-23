2b9bde5d6a56ed02d17b486b489fa3693482b5ab
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2023-02-23 16:04:17 -0300

    implement logic

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,22 @@ struct Args {
     search_term: String
 }
 
+#[derive(Debug)]
+struct FoundWord {
+    index: usize,
+    start: usize,
+    end: usize
+}
+
 impl Args {
     fn new(file_path: String, search_term: String) -> Args {
         Args { file_path, search_term }
+    }
+}
+
+impl FoundWord {
+    fn new(index: usize, start: usize, end: usize) -> FoundWord {
+        FoundWord { index, start, end }
     }
 }
 
@@ -29,7 +42,7 @@ fn main() {
         exit(1);
     }).unwrap();
 
-    let file_content = match get_file_contents(args.file_path) {
+    let file_content = match read_file_contents(args.file_path) {
         Ok(content) => content,
         Err(e) => {
             eprintln!("{e}");
@@ -37,7 +50,52 @@ fn main() {
         }
     };
 
-    println!("{file_content}");
+    let str_file_buffer: Vec<&str> = file_content.split(' ').collect();
+    let vec_of_words: Vec<String> = str_file_buffer.iter().map(|word| word.to_string()).collect();
+    
+    let vec_of_found_words = search_through_vec(args.search_term, &vec_of_words);
+
+    let mut text_highlighted = String::new();
+
+    vec_of_words.iter().enumerate().for_each(|(index, word)| {
+        for found_word in &vec_of_found_words {
+            if index == found_word.index {
+                let text_buffer = word.to_owned();
+                let (start_wrap, _) = text_buffer.split_at(found_word.start);
+                let (_, end_wrap) = text_buffer.split_at(found_word.end);
+                let highlighted_word = &text_buffer[found_word.start..found_word.end];
+
+                return text_highlighted.push_str(&format!("{start_wrap}\x1b[47m{highlighted_word}\x1b[0m{end_wrap} "));
+            }
+        }
+
+        text_highlighted.push_str(&format!("{word} "));
+    });
+
+    println!("{text_highlighted}");
+}
+
+fn search_through_vec(search_term: String, vec_of_words: &Vec<String>) -> Vec<FoundWord> {
+    let mut vec_of_found_words: Vec<FoundWord> = Vec::new();
+
+    for (index, word) in vec_of_words.iter().enumerate() {
+        if word.len() >= search_term.len() {
+            let times_to_loop = word.len() - search_term.len() + 1;
+            let mut end = search_term.len();
+
+            for start in 0..times_to_loop {
+                let slice = &word[start..end];
+
+                if slice.to_lowercase() == search_term.to_lowercase() {
+                    let found_word = FoundWord::new(index, start, end);
+                    vec_of_found_words.push(found_word);
+                }
+                end += 1;
+            }
+        }
+    }
+    
+    vec_of_found_words
 }
 
 fn get_args() -> Result<Args, Box<dyn CustomError>> {
@@ -56,7 +114,7 @@ fn get_args() -> Result<Args, Box<dyn CustomError>> {
     Ok(Args::new(file_path.to_owned(), search_term.to_owned()))
 }
 
-fn get_file_contents(file_path: String) -> Result<String, FileError> {
+fn read_file_contents(file_path: String) -> Result<String, FileError> {
     let mut file = File::open(file_path).map_err(|e| FileError::FileOpenError(e))?;
     let mut contents = String::new();
     
